eab9223c26d1d8b770813673a8455d1bcbdea063
Raj Patel <rpp.github@gmail.com>
2020-05-03 18:17:27 -0400

    trying to get example working

---
+++ dev/test.js
@@ -0,0 +1,15 @@
+
+const Sim = require('./../sim');
+// import * as Sim from './../sim'
+stream = new Sim.BattleStream();
+
+(async () => {
+    let output;
+    while ((output = await stream.read())) {
+        console.log(output);
+    }
+})();
+
+stream.write(`>start {"formatid":"gen7randombattle"}`);
+stream.write(`>player p1 {"name":"Alice"}`);
+stream.write(`>player p2 {"name":"Bob"}`);
